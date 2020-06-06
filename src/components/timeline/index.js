cf7f014cce5275e14c8e0ea99c9c4d2258b5855c
tarun <tarunrajkhanna@gmail.com>
2020-06-07 01:36:07 +0530

    animejs replaced by css animations in timeline

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,13 +1,11 @@
 import React, { useRef, useEffect, useState } from 'react';
 import styles from './index.module.css';
 import clsx from 'clsx';
-import anime from 'animejs';
 import withHeroScrollState from '../../hoc/heroScrollStateHOC';
 import officeImg1 from '../../assets/images/office1.jpg';
 import officeImg2 from '../../assets/images/office2.jpg';
 import officeImg3 from '../../assets/images/office3.jpg';
 import { getPlatform } from '../../helpers';
-import { usePrevious } from '../../hooks/customHooks';
 
 
 const Timeline = ({
@@ -16,94 +14,8 @@ const Timeline = ({
   reverse
 }) => {
   const [animationPlay, setAnimationPlay] = useState(false);
-  const prevAnimationPlay = usePrevious(animationPlay);
-  const timelineBox1 = useRef(null);
-  const timelineBox2 = useRef(null);
-  const timelineBox3 = useRef(null);
-  const polaroid1 = useRef(null);
-  const polaroid2 = useRef(null);
-  const polaroid3 = useRef(null);
   const timelinePageRef = useRef(null);
   let observer = null;
-
-
-  const reverseGalleryAnimation = () => {
-    const animeTimeline = anime.timeline({
-      duration: 4000,
-    });
-    animeTimeline.add({
-      targets: polaroid1.current,
-      translateX: 780,
-      rotate: [0, 10],
-      easing: 'spring(1, 80, 50, 0)'
-    });
-    animeTimeline.add({
-      targets: polaroid2.current,
-      translateX: 680,
-      rotate: [0, -20],
-      easing: 'spring(1, 80, 50, 0)'
-    }, '-=500');
-    animeTimeline.add({
-      targets: polaroid3.current,
-      translateX: 660,
-      translateY: 60,
-      rotate: [0, 20],
-      easing: 'spring(1, 80, 50, 0)'
-    }, '-=500');
-  };
-
-  const galleryAnimation = () => {
-    const animeTimeline = anime.timeline({
-      duration: 4000,
-    });
-    animeTimeline.add({
-      targets: polaroid1.current,
-      translateX: -780,
-      rotate: [0, -10],
-      easing: 'spring(1, 80, 50, 0)'
-    });
-    animeTimeline.add({
-      targets: polaroid2.current,
-      translateX: -680,
-      rotate: [0, 20],
-      easing: 'spring(1, 80, 50, 0)'
-    }, '-=500');
-    animeTimeline.add({
-      targets: polaroid3.current,
-      translateX: -680,
-      translateY: 60,
-      rotate: [0, -20],
-      easing: 'spring(1, 80, 50, 0)'
-    }, '-=500');
-  };
-
-  const timelineAnimation = () => {
-    const animTimeline = anime.timeline({
-      duration: 1000,
-      easing: 'easeInOutExpo'
-    });
-    animTimeline.add({
-      targets: timelineBox1.current,
-      translateY: [100, 0],
-      opacity: [0, 1],
-      easing: 'easeInOutExpo',
-      duration: 800,
-    });
-    animTimeline.add({
-      targets: timelineBox2.current,
-      opacity: [0, 1],
-      easing: 'easeInOutExpo',
-      duration: 800,
-      translateY: [100, 0],
-    }, '-=400');
-    animTimeline.add({
-      targets: timelineBox3.current,
-      opacity: [0, 1],
-      easing: 'easeInOutExpo',
-      duration: 800,
-      translateY: [100, 0]
-    }, '-=400');
-  };
 
   const intersectionListener = (entries) => {
     entries.forEach(entry => {
@@ -116,29 +28,83 @@ const Timeline = ({
   };
 
   useEffect(() => {
-    if (animationPlay && animationPlay !== prevAnimationPlay) {
-      timelineAnimation();
-      if (reverse && getPlatform().desktop) { reverseGalleryAnimation(); } else galleryAnimation();
-    }
-  }, [animationPlay]);
-
-  useEffect(() => {
     const options = {
-      threshold: [0.25, 0.50, 0.75, 0.90]
+      threshold: [0.50]
     };
     observer = new IntersectionObserver(intersectionListener, options);
     observer.observe(timelinePageRef.current);
   }, []);
 
 
+  const getPolariod1Styles = () => {
+    if (!animationPlay) return null;
+    let polaroid1Styles = {
+      transitionDelay: 0,
+      willChange: 'transform',
+    }
+    if (reverse) {
+      polaroid1Styles = {
+        ...polaroid1Styles,
+        transform: 'translateX(780px) rotate(10deg)'
+      }
+    } else {
+      polaroid1Styles = {
+        ...polaroid1Styles,
+        transform: 'translateX(-780px) rotate(-10deg)'
+      }
+    }
+    return polaroid1Styles;
+  }
+
+  const getPolariod2Styles = () => {
+    if (!animationPlay) return null;
+    let polaroid2Styles = {
+      transitionDelay: '400ms',
+      willChange: 'transform',
+    }
+    if (reverse) {
+      polaroid2Styles = {
+        ...polaroid2Styles,
+        transform: 'translateX(680px) rotate(-20deg)'
+      }
+    } else {
+      polaroid2Styles = {
+        ...polaroid2Styles,
+        transform: 'translateX(-680px) rotate(20deg)'
+      }
+    }
+    return polaroid2Styles;
+  }
+
+
+  const getPolariod3Styles = () => {
+    if (!animationPlay) return null;
+    let polaroid3Styles = {
+      transitionDelay: '800ms',
+      willChange: 'transform',
+    }
+    if (reverse) {
+      polaroid3Styles = {
+        ...polaroid3Styles,
+        transform: 'translate(680px, 60px) rotate(20deg)'
+      }
+    } else {
+      polaroid3Styles = {
+        ...polaroid3Styles,
+        transform: 'translate(-680px, 60px) rotate(-20deg)'
+      }
+    }
+    return polaroid3Styles;
+  }
+
   return (
-    <div ref={timelinePageRef} style={{ display: 'flex' }} className="section_container">
+    <div ref={timelinePageRef} style={{ display: 'flex' }} className="section_container" >
       <div className={clsx([styles.contentContainer, reverse && getPlatform().desktop && styles.reverseContentContainer])}>
         <p className="section_heading">{heading}</p>
         <div className={clsx([styles.timeline, animationPlay ? styles.ruler : null])}>
           {timelineItems[0] ? (
             <div className={styles.timelineBoxContainer}>
-              <div ref={timelineBox1} className={clsx([styles.timelineBox])}>
+              <div className={clsx([styles.timelineBox, styles.timelineBox1, animationPlay && styles.animateTimelineBox])}>
                 <p className={styles.timelineBoxHeading}>{timelineItems[0].title}</p>
                 <p className={styles.timelineBoxDuration}>{timelineItems[0].duration}</p>
                 <p className={styles.timelineBoxInfo}>
@@ -150,7 +116,7 @@ const Timeline = ({
 
           {timelineItems[1] ? (
             <div className={styles.timelineBoxContainer}>
-              <div ref={timelineBox2} className={clsx([styles.timelineBox])}>
+              <div className={clsx([styles.timelineBox, styles.timelineBox2, animationPlay && styles.animateTimelineBox])}>
                 <p className={styles.timelineBoxHeading}>{timelineItems[1].title}</p>
                 <p className={styles.timelineBoxDuration}>{timelineItems[1].duration}</p>
                 <p className={styles.timelineBoxInfo}>
@@ -162,7 +128,7 @@ const Timeline = ({
 
           {timelineItems[2] ? (
             <div className={styles.timelineBoxContainer}>
-              <div ref={timelineBox3} className={clsx([styles.timelineBox])}>
+              <div className={clsx([styles.timelineBox, styles.timelineBox3, animationPlay && styles.animateTimelineBox])}>
                 <p className={styles.timelineBoxHeading}>{timelineItems[2].title}</p>
                 <p className={styles.timelineBoxDuration}>{timelineItems[2].duration}</p>
                 <p className={styles.timelineBoxInfo}>
@@ -174,19 +140,30 @@ const Timeline = ({
 
         </div>
       </div>
-      {getPlatform().desktop ? (
-        <div className={styles.galleryContainer}>
-          <div ref={polaroid1} className={clsx([styles.polaroidBox, reverse && getPlatform().desktop && styles.reversePolaroidBox])}>
-            <img src={officeImg1} className={styles.polaroidImg} />
+      {
+        getPlatform().desktop ? (
+          <div className={styles.galleryContainer}>
+            <div
+              className={clsx([styles.polaroidBox, reverse && getPlatform().desktop && styles.reversePolaroidBox])}
+              style={getPolariod1Styles()}
+            >
+              <img src={officeImg1} className={styles.polaroidImg} />
+            </div>
+            <div
+              className={clsx([styles.polaroidBox, reverse && getPlatform().desktop && styles.reversePolaroidBox])}
+              style={getPolariod2Styles()}
+            >
+              <img src={officeImg2} className={styles.polaroidImg} />
+            </div>
+            <div
+              className={clsx([styles.polaroidBox, reverse && getPlatform().desktop && styles.reversePolaroidBox])}
+              style={getPolariod3Styles()}
+            >
+              <img src={officeImg3} className={styles.polaroidImg} />
+            </div>
           </div>
-          <div ref={polaroid2} className={clsx([styles.polaroidBox, reverse && getPlatform().desktop && styles.reversePolaroidBox])}>
-            <img src={officeImg2} className={styles.polaroidImg} />
-          </div>
-          <div ref={polaroid3} className={clsx([styles.polaroidBox, reverse && getPlatform().desktop && styles.reversePolaroidBox])}>
-            <img src={officeImg3} className={styles.polaroidImg} />
-          </div>
-        </div>
-      ) : null}
+        ) : null
+      }
     </div>
   );
 };
